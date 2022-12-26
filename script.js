5628795c2c0caa3ef9490f4317ddcb2b424c6e8c
Melon <99061680+Mel0n7@users.noreply.github.com>
2022-12-26 16:40:08 +0000

    Fixed #6 and #7

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ var domain = document.domain
 if (domain.includes("repl.co")){
   document.title = "FluentOS - Website Test"
 }
+if (window.localStorage.getItem('darkMode')){
+  document.body.classList.add("dark")
+} else if (window.localStorage.getItem('darkMode') === null) {
+  window.localStorage.setItem('darkMode', true)
+  document.body.classList.add("dark")
+}
