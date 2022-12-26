4ab0987f7233b6b7c3a843df6db23509b63ed4b2
Melon <99061680+Mel0n7@users.noreply.github.com>
2022-12-26 17:25:18 +0000

    #8

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ var domain = document.domain
 if (domain.includes("repl.co")){
   document.title = "FluentOS - Website Test"
 }
-if (window.localStorage.getItem('darkMode')){
+if (window.localStorage.getItem('darkMode') == "true"){
   document.body.classList.add("dark")
 } else if (window.localStorage.getItem('darkMode') === null) {
   window.localStorage.setItem('darkMode', true)
   document.body.classList.add("dark")
+}
+function toggleTheme(){
+  if (window.localStorage.getItem('darkMode') == "true"){
+    window.localStorage.setItem('darkMode', false)
+    document.body.classList.remove("dark")
+  } else {
+    window.localStorage.setItem('darkMode', true)
+    document.body.classList.add("dark")
+  }
 }
